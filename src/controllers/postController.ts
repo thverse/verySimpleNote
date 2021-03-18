535cd52ebd1d97311ef4037735166c13e2bd765e
kim tae-hyun <wowo1127@naver.com>
2021-03-18 12:36:59 +0900

    deleted 컬럼 추가

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -19,6 +19,15 @@ export const getPost = async (req: Request, res: Response): Promise<Response> =>
     return res.json(result[0]);
 }
 
+export const deletePost = async (req: Request, res: Response): Promise<Response> => {
+
+    const conn = await connect();
+    const postId: string = req.params.postId 
+    const result = await conn.query(`SELECT * FROM posts WHERE id = ?`, [postId]);
+
+    return res.json(result[0]);
+}
+
 export const createPost = async (req: Request, res: Response): Promise<Response> => {
     const newPost: Post = req.body;
     const conn = await connect();
